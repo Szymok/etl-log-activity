1074845d232178463a7dd4f02f41feec8bd48dc4
Szymon Kowalewski <37596854+Szymok@users.noreply.github.com>
2022-10-31 00:45:59 +0000

    create basic mysql connection

---
+++ simulate_orders.py
@@ -0,0 +1,23 @@
+import pymysql
+import configparser
+
+# Creating a parser object and reading from the config file
+parser = configparser.ConfigParser()
+parser.read('pipeline.conf')
+
+# Grabing the values from the config file
+hostname = parser.get('mysql_config', 'hostname')
+port = parser.get('mysql_config', 'port')
+username = parser.get('mysql_config', 'username')
+password = parser.get('mysql_config', 'password')
+dbname = parser.get('mysql_config', 'database')
+
+# Connecting
+print('Connecting to MySQL')
+conn = pymysql.connect(host=hostname,
+                       user=username,
+                       password=password,
+                       db=dbname,
+                       port=int(port))
+
+cursor = conn.cursor()
